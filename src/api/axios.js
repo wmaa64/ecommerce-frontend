0a759625f2686e3fa37d94bf24030a8d3c5286ff
wael mohammed aly <wmaa64@yahoo.com>
2024-09-13 16:56:40 +0300

    Update backend base URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',  // Base URL for API requests
+  //baseURL: 'http://localhost:5000',  // Base URL for API requests
+  baseURL: 'https://ecommerce-backend-evl1587ss-wael-alys-projects.vercel.app',
 });
 
 export default axiosInstance;
