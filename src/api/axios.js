7eb12300300d54eb7a640546a01071f12d1c03af
wael mohammed aly <wmaa64@yahoo.com>
2024-09-13 21:43:27 +0300

    Your commit message here

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
+  baseURL: 'https://ecommerce-backend-v1tg-2hq4ui5bj-wael-alys-projects.vercel.app'
   //baseURL: 'http://localhost:5000',  // Base URL for API requests
-  baseURL: 'https://ecommerce-backend-evl1587ss-wael-alys-projects.vercel.app',
-  
+  //baseURL: 'https://ecommerce-backend-evl1587ss-wael-alys-projects.vercel.app',
 });
 
 export default axiosInstance;
