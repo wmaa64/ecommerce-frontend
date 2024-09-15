65178dd659a3d31b62a8e32c1e153c3b31be1ddd
wael mohammed aly <wmaa64@yahoo.com>
2024-09-15 23:40:06 +0300

    new version one

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL:  'https://ecommerce-backend-danqxn8xe-wael-alys-projects.vercel.app'
+  baseURL:  'https://ecommerce-backend-3zn2fmzmc-wael-alys-projects.vercel.app'
 });
 
 export default axiosInstance;
