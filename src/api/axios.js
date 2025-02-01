54958c5f758aab81deaf2b59baf33201bdb58d1e
wael mohammed aly <wmaa64@yahoo.com>
2025-02-01 13:00:26 +0200

    2nd 1-2-2025

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   //baseURL: 'http://localhost:5000'  
   baseURL: 'https://ecommerce-backend-z4v3.onrender.com'
+
 });
 
 export default axiosInstance;
